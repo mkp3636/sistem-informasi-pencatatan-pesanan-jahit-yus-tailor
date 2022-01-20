9d436719dbd209966a7f802083025e000a64a172
mkp3636 <74285084+mkp3636@users.noreply.github.com>
2022-01-20 16:47:03 +0700

    Update celana_edit.php

--- views/celana_edit.php
+++ views/celana_edit.php
@@ -167,7 +167,7 @@ $data= mysqli_fetch_array($ambil);
                         <div class="form-group">
                             <div class="col-sm-offset-3 col-sm-9">
                                 <button type="submit" class="btn btn-success">
-                                    <span class="fa fa-edit"></span> Update Data Arsip</button>
+                                    <span class="fa fa-edit"></span> Update Data Celana</button>
                             </div>
                         </div>
                     </form>
